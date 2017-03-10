fb6250d715dd0ad58ab42d7e726042b65f8eb52b
chenmeiyi <chenmeiyi@supermap.com>
2017-03-10 18:12:02 +0800

    修改服务器地址。 review by zhuch

--- test/Tool/GlobeParameter.js
+++ test/Tool/GlobeParameter.js
@@ -1,20 +1,20 @@
-﻿//CI测试机的地址为：http://localhost:8090
+﻿//CI测试机的地址为：http://192.168.112.222:8090
 GlobeParameter = {
-    rootURL: "http://localhost:8090/iserver/services",
-    mapServiceURL: "http://localhost:8090/iserver/services/map-world/rest/maps/",
-    worldMapURL: "http://localhost:8090/iserver/services/map-world/rest/maps/世界地图",
-    networkAnalystURL: "http://localhost:8090/iserver/services/transportationanalyst-sample/rest/networkanalyst/RoadNet@Changchun",
-    spatialAnalystURL: "http://localhost:8090/iserver/services/spatialanalyst-sample/restjsr/spatialanalyst",
-    spatialAnalystURL_Changchun:"http://localhost:8090/iserver/services/spatialanalyst-changchun/restjsr/spatialanalyst",
-    dataServiceURL: "http://localhost:8090/iserver/services/data-world/rest/data",
-    editServiceURL: "http://localhost:8090/iserver/services/data-jingjin/rest/data/datasources/name/Jingjin/datasets/name/Landuse_R",
-    trafficTransferURL: "http://localhost:8090/iserver/services/traffictransferanalyst-sample/restjsr/traffictransferanalyst/Traffic-Changchun",
-	vectorURL: "http://localhost:8090/iserver/services/map-world/rest/maps/World/",
-    gridURL:"http://localhost:8090/iserver/services/map-jingjin/rest/maps/京津地区土地利用现状图",
-	imageURL: "http://localhost:8090/iserver/services/map-world/rest/maps/世界地图_Day",
-	WorldURL: "http://localhost:8090/iserver/services/map-world/rest/maps/World",
-    ChinaURL:"http://localhost:8090/iserver/services/map-china400/rest/maps/China",
-    WMTSURL:"http://localhost:8090/iserver/services/map-world/wmts100",
-	WCSURL:  "http://localhost:8090/iserver/services/data-world/wcs111",
-    plotUrl:  "http://localhost:8090/iserver/services/plot-jingyong/rest/plot"
+    rootURL: "http://192.168.112.222:8090/iserver/services",
+    mapServiceURL: "http://192.168.112.222:8090/iserver/services/map-world/rest/maps/",
+    worldMapURL: "http://192.168.112.222:8090/iserver/services/map-world/rest/maps/世界地图",
+    networkAnalystURL: "http://192.168.112.222:8090/iserver/services/transportationanalyst-sample/rest/networkanalyst/RoadNet@Changchun",
+    spatialAnalystURL: "http://192.168.112.222:8090/iserver/services/spatialanalyst-sample/restjsr/spatialanalyst",
+    spatialAnalystURL_Changchun:"http://192.168.112.222:8090/iserver/services/spatialanalyst-changchun/restjsr/spatialanalyst",
+    dataServiceURL: "http://192.168.112.222:8090/iserver/services/data-world/rest/data",
+    editServiceURL: "http://192.168.112.222:8090/iserver/services/data-jingjin/rest/data/datasources/name/Jingjin/datasets/name/Landuse_R",
+    trafficTransferURL: "http://192.168.112.222:8090/iserver/services/traffictransferanalyst-sample/restjsr/traffictransferanalyst/Traffic-Changchun",
+	vectorURL: "http://192.168.112.222:8090/iserver/services/map-world/rest/maps/World/",
+    gridURL:"http://192.168.112.222:8090/iserver/services/map-jingjin/rest/maps/京津地区土地利用现状图",
+	imageURL: "http://192.168.112.222:8090/iserver/services/map-world/rest/maps/世界地图_Day",
+	WorldURL: "http://192.168.112.222:8090/iserver/services/map-world/rest/maps/World",
+    ChinaURL:"http://192.168.112.222:8090/iserver/services/map-china400/rest/maps/China",
+    WMTSURL:"http://192.168.112.222:8090/iserver/services/map-world/wmts100",
+	WCSURL:  "http://192.168.112.222:8090/iserver/services/data-world/wcs111",
+    plotUrl:  "http://192.168.112.222:8090/iserver/services/plot-jingyong/rest/plot"
 };
