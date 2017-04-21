00e665a32f5e84903a8d6c3e3ffbb20dac9f1284
songyumeng <songyumeng@supermap.com>
2017-04-21 15:08:02 +0800

    增加 mapboxg例子

---
+++ examples/mapboxgl/config.js
@@ -0,0 +1,21 @@
+/**
+ * Leaflet 示例配置文件：包含示例的分类、名称、缩略图、文件路径
+ */
+var exampleConfig = {
+    "iServer": {
+        name: "iServer",
+        content: {
+            "map": {
+                name: "地图",
+                content: [
+                    {
+                        name: "3857底图",
+                        thumbnail: "mb_TileLayer.png",
+                        fileName: "01_tiledMapLayer"
+                    }
+                ]
+            }
+
+        }
+    }
+};
