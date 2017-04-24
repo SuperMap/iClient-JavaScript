3f12c20b6df5c0a7dd0355acdfc75ef6a9b01000
songyumeng <songyumeng@supermap.com>
2017-04-24 13:56:59 +0800

    尝试修改测试

--- src/common/util/Request.js
+++ src/common/util/Request.js
@@ -1,5 +1,5 @@
-require("whatwg-fetch/fetch");
-var fetchJsonp = require('fetch-jsonp/build/fetch-jsonp');
+require("whatwg-fetch");
+var fetchJsonp = require('fetch-jsonp');
 
 SuperMap.Request = SuperMap.Class({
 
