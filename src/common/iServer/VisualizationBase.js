497032ae9c62c3c695eb46f46493946c871cfcde
songyumeng <songyumeng@supermap.com>
2017-08-05 19:42:27 +0800

    fix

---
+++ src/common/iServer/VisualizationBase.js
@@ -0,0 +1,1 @@
+import '../../legacy/libs/SuperMap_Visualization-8.1.1-15125.js';
---
+++ src/leaflet/overlay/VectorTileFormat.js
@@ -0,0 +1,10 @@
+/**
+ * 矢量瓦片格式
+ */
+import L from "leaflet";
+export var VectorTileFormat = {
+    JSON: "JSON",
+    MVT: "MVT",
+    PBF: "PBF"
+};
+L.supermap.VectorTileFormat = VectorTileFormat;
