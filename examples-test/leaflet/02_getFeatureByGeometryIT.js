8a368b4dcdd6a3739bf00c03a0013703311e2ebe
chenmeiyi <chenmeiyi@supermap.com>
2017-09-05 09:52:48 +0800

    修改leaflet数据集几何查询示例及对应的VT。

--- examples-test/leaflet/02_getFeatureByGeometryIT.js
+++ examples-test/leaflet/02_getFeatureByGeometryIT.js
@@ -7,12 +7,23 @@ module.exports = {
         /*check elements exist*/
         browser.waitForElementPresent('.leaflet-pane.leaflet-overlay-pane', 10000);
         browser.waitForElementPresent('.leaflet-pane.leaflet-overlay-pane canvas', 10000);
-        //browser.waitForElementPresent('.leaflet-popup-content', 10000);
-        //var popupContent = '空间查询模式：INTERSECT';
-        //browser.expect.element('.leaflet-popup-content').text.to.be.contain(popupContent);
-        //browser.click('.leaflet-popup-close-button', function () {
-        //    browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
-        //});
+        browser.waitForElementPresent('.leaflet-popup-content', 10000);
+        var popupContent = '空间查询模式：INTERSECT';
+        browser.expect.element('.leaflet-popup-content').text.to.be.contain(popupContent);
+        browser.click('.leaflet-popup-close-button', function () {
+            browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
+        });
+        browser.pause(1000);
+        browser.doubleClick(function () {
+            browser.waitForElementPresent('.leaflet-popup-content-wrapper', 10000);
+            browser.waitForElementPresent('.leaflet-popup-content', 10000);
+            var queryInfo = "国家：扎伊尔";
+            browser.expect.element('.leaflet-popup-content').text.to.be.contain(queryInfo);
+        });
+        browser.pause(1000);
+        browser.click('.leaflet-popup-close-button', function () {
+            browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
+        });
         browser.pause(1000);
         browser.end();
     }
