af4c51a6093c2f5f98d1adac6c5e7b41f100b9bd
chenmeiyi <chenmeiyi@supermap.com>
2017-10-27 13:40:34 +0800

    fix UT

--- test/leaflet/mapping/ImageMapLayerSpec.js
+++ test/leaflet/mapping/ImageMapLayerSpec.js
@@ -55,6 +55,6 @@ describe('leaflet_ImageMapLayer', function () {
         expect(imageLayerObject.options.prjCoordSys.epsgCode).toBe(4326);
         var coords = L.point(120.14, 30.24);
         var tileUrl = imageLayerObject.getTileUrl(coords);
-        expect(tileUrl).toBe(url + "/image.png?width=256&height=256&redirect=true&transparent=false&cacheEnabled=true&prjCoordSys=%7B%22epsgCode%22:4326%7D&overlapDisplayed=true&viewBounds=%7B%22leftBottom%22%20:%20%7B%22x%22:2387268743.959911,%22y%22:-20037508.34278071%7D,%22rightTop%22%20:%20%7B%22x%22:2407306252.3027,%22y%22:-20037508.34278071%7D%7D");
+        expect(tileUrl).toBe(GlobeParameter.mapServiceURL + "%E4%B8%96%E7%95%8C%E5%9C%B0%E5%9B%BE_Day/image.png?width=256&height=256&redirect=true&transparent=false&cacheEnabled=true&prjCoordSys=%7B%22epsgCode%22:4326%7D&overlapDisplayed=true&viewBounds=%7B%22leftBottom%22%20:%20%7B%22x%22:2387268743.959911,%22y%22:-20037508.34278071%7D,%22rightTop%22%20:%20%7B%22x%22:2407306252.3027,%22y%22:-20037508.34278071%7D%7D");
     });
 });
