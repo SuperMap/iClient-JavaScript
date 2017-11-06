d1878c8b83e6355d27c6e239bb9adc67da2cb447
chenmeiyi <chenmeiyi@supermap.com>
2017-11-06 16:38:31 +0800

    fix VT

---
+++ examples-test/base/ExampleTestGlobeParameter.js
@@ -0,0 +1,5 @@
+﻿GlobeParameter = {
+    datajingjinURL: "http://localhost:8090/iserver/services/data-jingjin/rest/data/datasources/Jingjin/datasets/",
+    datachangchunURL: "http://localhost:8090/iserver/services/data-changchun/rest/data/datasources/Changchun/datasets/",
+    dataspatialAnalystURL: "http://localhost:8090/iserver/services/data-spatialAnalyst/rest/data/datasources/Interpolation/datasets/"
+};
