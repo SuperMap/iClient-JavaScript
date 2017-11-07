c489b1f9c786c856098fec74cf6afce1927dcc6b
zhurc <zhurongchang@supermap.com>
2017-11-07 18:40:36 +0800

    fix 漏修改注释

--- src/openlayers/overlay/Label.js
+++ src/openlayers/overlay/Label.js
@@ -474,7 +474,7 @@ export default class Label extends GeoFeature {
      * @description 获得标签要素的最终范围的另一种算法（通过记录下的标签宽高），提高计算bounds的效率。
      *
      * @param feature - {SuperMap.Feature.Vector} 需要计算bounds的标签要素数。
-     * @param loc - {ol.geom.Point} 标签位置
+     * @param loc - {Object} 标签位置,例如：{"x":1,"y":1}
      *
      * @return {Array<Object>}  四边形节点数组。例如：[{"x":1,"y":1},{"x":3,"y":1},{"x":6,"y":4},{"x":2,"y":10},{"x":1,"y":1}]。
      */
