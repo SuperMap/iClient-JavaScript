80c50b7fa776bde77aea47514b4b104384410ea3
huangqh <huangqinghua@supermap.com>
2017-12-21 16:52:14 +0800

    修改 上一次提交解决分段专题图判断最大值时没有包含等于时代码写的不正确问题。

--- src/leaflet/overlay/RangeThemeLayer.js
+++ src/leaflet/overlay/RangeThemeLayer.js
@@ -75,7 +75,8 @@ export var RangeThemeLayer = GeoFeatureThemeLayer.extend({
         //判断属性值是否属于styleGroups的某一个范围，以便对获取分组 style
         if (isSfInAttributes && isValidStyleGroup) {
             for (var i = 0, len = groups.length; i < len; i++) {
-                if ((attribute >= groups[i].start) && (attribute <= groups[i].end)) {
+				var isContianed = i === len-1 ? ((attribute >= groups[i].start) && (attribute <= groups[i].end)) : ((attribute >= groups[i].start) && (attribute < groups[i].end));
+                if (isContianed) {
                     var sty1 = groups[i].style;
                     style = CommonUtil.copyAttributesWithClip(style, sty1);
                 }
