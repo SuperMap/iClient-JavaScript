4bbc32d41e56f85973878483657de75aa2c4e258
songyumeng <songyumeng@supermap.com>
2018-02-06 17:42:07 +0800

    优化jsdoc 侧边栏搜索速度

--- build/jsdocs/template/static/scripts/main.js
+++ build/jsdocs/template/static/scripts/main.js
@@ -7,34 +7,35 @@ $(function () {
             var li = $('.sidebar-menu').find('li');
             if (term.length === 0) {
                 li.each(function () {
-                    var el=$(this);
-                    el.show(0);
+                    var el = $(this);
+                    el.css("display", "block");
                     el.removeClass('active');
-                    if (el.data('lte.pushmenu.active')) {
-                        el.addClass('active');
-                    }
                 });
+                $('.sidebar-menu').find('.itemMembers').hide();
                 return;
             }
             var regexp = new RegExp(term, 'i');
-            
+
             li.hide();
             li.removeClass('pushmenu-search-found', false);
             li.removeClass('active');
-            var start = new Date();
             li.each(function () {
-                var el=$(this);
+                var el = $(this);
                 if (el.text() && regexp.test(el.text())) {
-                    el.show();
+                    el.css("display", "block");
                     el.addClass('pushmenu-search-found');
 
                     if (el.is('.treeview')) {
                         el.addClass('active');
                     }
 
-                    var parent =el.parents('li').first();
+                    var parent = el.parents('li').first();
                     if (parent.is('.treeview')) {
                         parent.show();
+                    }
+                    var parentUl = el.parents('ul').first();
+                    if (parentUl.is('.treeview-menu')) {
+                        parentUl.show();
                     }
 
                 }
@@ -45,7 +46,7 @@ $(function () {
             });
 
             $('.sidebar-menu li.pushmenu-search-found.treeview').each(function () {
-                var el=$(this);
+                var el = $(this);
                 el.find('.pushmenu-search-found').show();
                 el.find('.pushmenu-search-found').closest('.itemMembers').show();
             });
@@ -58,7 +59,6 @@ $(function () {
             $(this).parent().find('.itemMembers').toggle();
         });
         // // Show an item related a current documentation automatically
-        debugger
         var filename = $('.page-title').data('filename').replace(/\.[a-z]+$/, '');
         var $currentItem = $('.main-sidebar .item[data-name="' + filename + '"]:eq(0)');
 
@@ -83,6 +83,6 @@ $(function () {
         };
 
         $(window).on('resize', _onResize);
-            _onResize();
+        _onResize();
     });
 });
