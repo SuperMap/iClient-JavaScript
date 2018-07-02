574a2eeca03c0033108aed14ef41be32d1378c4f
zhurc <zhurongchang@supermap.com>
2018-07-02 22:45:22 +0800

    【fix】 漏提交

---
+++ src/leaflet/core/Attributions.js
@@ -0,0 +1,43 @@
+/**
+ * attribution版权相关配置
+ */
+
+let Attributions = {
+
+    Prefix: `<a href='http://leafletjs.com' title='A JS library for interactive maps'>Leaflet</a>
+                with <span>© <a href='http://iclient.supermap.io' title='SuperMap iClient' target='_blank'>SuperMap iClient</a></span>`,
+
+    Common: {
+        attribution: `Map Data <span>© <a href='http://support.supermap.com.cn/product/iServer.aspx' title='SuperMap iServer' target='_blank'>SuperMap iServer</a></span>`
+    },
+
+    Online: {
+        attribution: `Map Data <span>© <a href='http://www.supermapol.com' title='SuperMap Online' target='_blank'>SuperMap Online</a></span>`
+    },
+
+    ECharts: {
+        attribution: `© 2017 百度 ECharts`
+    },
+
+    MapV: {
+        attribution: `© 2017 百度 MapV `
+    },
+
+    Turf: {
+        attribution: `<span>© <a href='http://turfjs.org/' title='turfjs' target='_blank'>turfjs</a></span>`
+    },
+
+    Baidu: {
+        attribution: `Map Data © 2017 Baidu - GS(2016)2089号 - Data © 长地万方`
+    },
+
+    Cloud: {
+        attribution: `Map Data ©2014 SuperMap - GS(2014)6070号-data©Navinfo`
+    },
+
+    Tianditu: {
+        attribution: `Map Data <a href='http://www.tianditu.com' target='_blank'><img style='background-color:transparent;bottom:2px;opacity:1;' src='http://api.tianditu.com/img/map/logo.png' width='53px' height='22px' opacity='0'></a>`
+    }
+};
+
+export default Attributions
