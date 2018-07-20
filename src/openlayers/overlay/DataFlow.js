e3de7e07d0fdeed5270702143435355d21d43781
demaweiliya <qiwei@supermap.com>
2018-07-20 09:26:26 +0800

    【API】 ol下dataflow 参数类型错误修改

--- src/openlayers/overlay/DataFlow.js
+++ src/openlayers/overlay/DataFlow.js
@@ -7,7 +7,7 @@ import {DataFlowService} from "../services/DataFlowService";
  * @classdesc 实时数据图层源。
  * @param {Object} opt_options - 参数。
  * @param {string} [opt_options.idField = 'id']
- * @param {Array.{Object}} [opt_options.geometry] - 设置增添的几何要素对象数组。
+ * @param {Array.<Object>} [opt_options.geometry] - 设置增添的几何要素对象数组。
  * @param {Object} [opt_options.prjCoordSys] - 请求的地图的坐标参考系统。当此参数设置的坐标系统不同于地图的原有坐标系统时， 系统会进行动态投影，并返回动态投影后的地图瓦片。例如：{"epsgCode":3857}。
  * @param {Object} [opt_options.excludeField] - 排除字段
  * @extends {ol.source.Vector}
