dda36725b8ad6e5e4f3dc4deea0415534135e61d
songyumeng <songyumeng@supermap.com>
2018-09-27 18:27:27 +0800

    优化打包

--- build/publish.js
+++ build/publish.js
@@ -18,6 +18,26 @@ fs.readdir(path.join(filePath, key), function (err, files) {
             var readStream = fs.createReadStream(sourceFile);
             var writeStream = fs.createWriteStream(destPath);
             readStream.pipe(writeStream);
+            return;
+        }
+        //workers
+        if (fs.statSync(path.join(filePath, key, fileName)).isDirectory()) {
+            if (!fs.existsSync(path.join(__dirname, "../src/" + key + "/dist/", fileName))) {
+                fs.mkdirSync(path.join(__dirname, "../src/" + key + "/dist/", fileName));
+            }
+            fs.readdir(path.join(filePath, key, fileName), function (err, subfiles) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                subfiles.forEach(function (subfileName) {
+                    var sourceFile = path.join(__dirname, "../dist/", key, fileName, subfileName);
+                    var destPath = path.join(__dirname, "../src/" + key + "/dist/", fileName, subfileName);
+                    var readStream = fs.createReadStream(sourceFile);
+                    var writeStream = fs.createWriteStream(destPath);
+                    readStream.pipe(writeStream);
+                })
+            })
         }
     })
 })
