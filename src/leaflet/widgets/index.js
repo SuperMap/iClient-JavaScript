014fa88e125ed7a466c866efddf35c42f4d0c4d8
liqian1 <liqian1@supermap.com>
2018-12-13 17:50:54 +0800

    【fix】 fix 微件 index 文件名大小写错误 review by zhurc

--- src/leaflet/widgets/index.js
+++ src/leaflet/widgets/index.js
@@ -1,8 +1,8 @@
 /* Copyright© 2000 - 2018 SuperMap Software Co.Ltd. All rights reserved.
  * This program are made available under the terms of the Apache License, Version 2.0
  * which accompanies this distribution and is available at http://www.apache.org/licenses/LICENSE-2.0.html.*/
-import { OpenFileView, openFileView } from './openFile/OpenFileView';
-import { OpenFileViewModel, openFileViewModel } from './openFile/OpenFileViewModel';
+import { OpenFileView, openFileView } from './openfile/OpenFileView';
+import { OpenFileViewModel, openFileViewModel } from './openfile/OpenFileViewModel';
 import { SearchView, searchView } from './search/SearchView';
 import { searchViewModel, SearchViewModel } from './search/SearchViewModel';
 import { DataFlowView, dataFlowView } from './dataflow/DataFlowView';
