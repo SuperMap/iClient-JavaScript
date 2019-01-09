64c90d91379108ab035d3525ba8013d50621b656
yaoxiaoni <yaoxiaoni@supermap.com>
2019-01-09 13:59:25 +0800

    fix ci review by sunxiaoyu

--- test/openlayers/services/GeometryBatchAnalysisSpec.js
+++ test/openlayers/services/GeometryBatchAnalysisSpec.js
@@ -82,10 +82,10 @@ describe('openlayers_SpatialAnalystService_geometryBatchAnalysis', () => {
             expect(testUrl).toBe(serviceUrl + "/geometry/batchanalyst.json?returnContent=true&ignoreAnalystParam=true");
             var paramsObj = JSON.parse(params.replace(/'/g, "\""));
             expect(paramsObj[0].analystName).toBe("buffer");
-            expect(paramsObj[0].param.analystParameter.endType).toBe(ROUND);
+            expect(paramsObj[0].param.analystParameter.endType).toBe("ROUND");
             expect(paramsObj[0].param.analystParameter.leftDistance.value).toEqual(0.05);
             expect(paramsObj[1].analystName).toBe("overlay");
-            expect(paramsObj[0].param.operation).toBe("CLIP");
+            expect(paramsObj[1].param.operation).toBe("CLIP");
             expect(options).not.toBeNull();
             return Promise.resolve(new Response(JSON.stringify(geometryBatchAnalystEscapedJson)));
         });
