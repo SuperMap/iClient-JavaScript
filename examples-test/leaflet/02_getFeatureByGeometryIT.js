ea3077755ba76efa2c6883a77e6d3cce3364f849
songyumeng <songyumeng@supermap.com>
2019-08-30 11:17:20 +0800

    【fix】优化测试 review by liqian

--- examples-test/leaflet/02_getFeatureByGeometryIT.js
+++ examples-test/leaflet/02_getFeatureByGeometryIT.js
@@ -14,39 +14,23 @@ module.exports = {
             browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
         });
         browser.pause(1000);
-        browser.doubleClick(function () {
-            browser.waitForElementPresent('.leaflet-popup-content-wrapper', 10000);
-            browser.waitForElementPresent('.leaflet-popup-content', 10000);
-            var queryInfo = "国家：扎伊尔";
-            browser.expect.element('.leaflet-popup-content').text.to.be.contain(queryInfo);
+        browser.moveTo('map',560, 510, function () {
+            browser.pause(1000);
+            browser.doubleClick(function () {
+                browser.waitForElementPresent('.leaflet-popup-content-wrapper', 10000);
+                browser.waitForElementPresent('.leaflet-popup-content', 10000);
+                var queryInfo = "国家：安哥拉";
+                browser.expect.element('.leaflet-popup-content').text.to.be.contain(queryInfo);
+            });
+            browser.pause(1000);
+            browser.click('.leaflet-popup-close-button', function () {
+                browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
+            });
+            //测试版权点击的正确性
+            //commonTools.verifyCopyrightOfLeaflet(browser);
+            browser.pause(1000);
+            browser.end();
         });
-        browser.pause(1000);
-        browser.click('.leaflet-popup-close-button', function () {
-            browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
-        });
-        //测试版权点击的正确性
-        //commonTools.verifyCopyrightOfLeaflet(browser);
-        browser.pause(1000);
-        browser.end();
-
-        // chrome 76 Todo优化逻辑，兼容浏览器
-        // browser.moveTo( null,700, 700, function () {
-        //     browser.pause(1000);
-        //     browser.doubleClick(function () {
-        //         browser.waitForElementPresent('.leaflet-popup-content-wrapper', 10000);
-        //         browser.waitForElementPresent('.leaflet-popup-content', 10000);
-        //         var queryInfo = "国家：安哥拉";
-        //         browser.expect.element('.leaflet-popup-content').text.to.be.contain(queryInfo);
-        //     });
-        //     browser.pause(1000);
-        //     browser.click('.leaflet-popup-close-button', function () {
-        //         browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
-        //     });
-        //     //测试版权点击的正确性
-        //     //commonTools.verifyCopyrightOfLeaflet(browser);
-        //     browser.pause(1000);
-        //     browser.end();
-        // });
     }
 };
 
