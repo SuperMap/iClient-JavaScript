4ba50060685433c905235ba1b672d8d6ec419386
songyumeng <songyumeng@supermap.com>
2019-11-27 11:54:52 +0800

    Update tokengenerator.js

--- examples/js/tokengenerator.js
+++ examples/js/tokengenerator.js
@@ -1,1 +1,1 @@
-window.exampleToken = "fMSRxYQTkMfjlfP8Ce86nkgJEtX4Qx6XDp8kjq9kq1H9OizLar4SLugO2mTnC6X66Qhwl3-CaTNnoPVQkzfFRg.."
+window.exampleToken = "-VGEhY8FfvXsrb9U54p3mdFmOUBNEgeBjQaXYZMAuCQC7am-knhyIrFkGm-RiCnGkbCvIoosIhRXsI0VPe2Flg.."
