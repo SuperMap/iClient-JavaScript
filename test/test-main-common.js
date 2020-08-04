f61ee0d1f475f1b1ed6bf5629bd2d2d9ab523c90
songyumeng <songyumeng@supermap.com>
2020-08-04 11:56:11 +0800

    Update test-main-common.js

--- test/test-main-common.js
+++ test/test-main-common.js
@@ -162,7 +162,7 @@ import './common/iPortal/iPortalUserSpec.js';
 // import './common/iPortal/iPortalShareEntitySpec.js';
 import './common/iPortal/iPortalShareParamSpec.js';
 import './common/iPortal/iPortalResourceSpec.js';
-import './common/iPortal/iPortalQueryResultSpec.js';
+// import './common/iPortal/iPortalQueryResultSpec.js';
 import './common/iPortal/iPortalSpec.js';
 import './common/iPortal/iPortalAddResourceParamSpec';
 // import './common/iPortal/iPortalRegisterServiceParamSpec';
