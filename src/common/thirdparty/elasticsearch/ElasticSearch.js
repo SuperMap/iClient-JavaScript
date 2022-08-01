3c230c155e517b2a8a091d99b12748f38e5b0e31
luoxiao-supermap <luoxiao@supermap.com>
2022-08-01 11:09:08 +0800

    [fix]elastic doc修改 review by songym

--- src/common/thirdparty/elasticsearch/ElasticSearch.js
+++ src/common/thirdparty/elasticsearch/ElasticSearch.js
@@ -18,9 +18,13 @@ import {Util} from "../../commontypes/Util";
  * @param {Object} [options.geoFence] - 地理围栏。
  * @description 
  * <h3 style="font-size: 20px;margin-top: 20px;margin-bottom: 10px;">11.1.0</h3>
- * 该功能依赖<a href="https://github.com/elastic/elasticsearch">@elastic/elasticsearch</a>, webpack.5或其他不包含Node.js Polyfills的打包工具，需要加入相关配置，以webpack为例：
-   <pre><code>module.exports: {
+ * 该功能依赖<a href="https://github.com/elastic/elasticsearch">@elastic/elasticsearch</a>, webpack5或其他不包含Node.js Polyfills的打包工具，需要加入相关配置，以webpack为例：<br/>
+  <p style="margin-top:10px;">首先安装相关Polyfills</p><pre><code>npm i stream-http  https-browserify stream-browserify tty-browserify browserify-zlib os-browserify buffer url assert process -D</code></pre>
+  然后配置webpack<pre><code>module.exports: {
     resolve: {
+      alias: {
+        process: 'process/browser',
+      },
       mainFields: ['browser', 'main'],
       fallback: {
         fs: false,
@@ -32,6 +36,12 @@ import {Util} from "../../commontypes/Util";
         zlib: require.resolve('browserify-zlib')
       }
     }
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      }),
+    ]
 }</code></pre>
  * @usage
  */
