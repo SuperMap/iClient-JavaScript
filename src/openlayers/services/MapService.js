0367cb4ed2b61bccde25606a618f11cae9e975d7
qiwei <qiwei@supermap.com>
2022-10-18 16:21:21 +0800

    common mapcalculateutil 新增方法 review by luox

--- src/openlayers/services/MapService.js
+++ src/openlayers/services/MapService.js
@@ -1,10 +1,9 @@
 /* Copyright© 2000 - 2022 SuperMap Software Co.Ltd. All rights reserved.
  * This program are made available under the terms of the Apache License, Version 2.0
  * which accompanies this distribution and is available at http://www.apache.org/licenses/LICENSE-2.0.html.*/
-import {ServiceBase} from './ServiceBase';
+import { ServiceBase } from './ServiceBase';
 import { MapService as CommonMapService } from '@supermap/iclient-common/iServer/MapService';
 import { TilesetsService } from '@supermap/iclient-common/iServer/TilesetsService';
-
 /**
  * @class MapService
  * @category  iServer Map
@@ -23,52 +22,69 @@ import { TilesetsService } from '@supermap/iclient-common/iServer/TilesetsServic
  * @usage
  */
 export class MapService extends ServiceBase {
+  constructor(url, options) {
+    super(url, options);
+  }
 
-    constructor(url, options) {
-        super(url, options);
-    }
+  /**
+   * @function MapService.prototype.getMapInfo
+   * @description 地图信息查询服务。
+   * @param {RequestCallback} callback - 回调函数。
+   * @returns {MapService} 获取服务信息。
+   */
+  getMapInfo(callback) {
+    var me = this;
+    var getMapStatusService = new CommonMapService(me.url, {
+      proxy: me.options.proxy,
+      withCredentials: me.options.withCredentials,
+      crossOrigin: me.options.crossOrigin,
+      headers: me.options.headers,
+      eventListeners: {
+        scope: me,
+        processCompleted: callback,
+        processFailed: callback
+      },
+      projection: me.options.projection
+    });
+    getMapStatusService.processAsync();
+  }
 
-    /**
-     * @function MapService.prototype.getMapInfo
-     * @description 地图信息查询服务。
-     * @param {RequestCallback} callback - 回调函数。
-     * @returns {MapService} 获取服务信息。
-     */
-    getMapInfo(callback) {
-        var me = this;
-        var getMapStatusService = new CommonMapService(me.url, {
-            proxy: me.options.proxy,
-            withCredentials: me.options.withCredentials,
-            crossOrigin: me.options.crossOrigin,
-            headers: me.options.headers,
-            eventListeners: {
-                scope: me,
-                processCompleted: callback,
-                processFailed: callback
-            }, projection: me.options.projection
-        });
-        getMapStatusService.processAsync();
-    }
+  getWkt(callback) {
+    var me = this;
+    var getMapStatusService = new CommonMapService(`${me.url}/prjCoordSys.wkt`, {
+        proxy: me.options.proxy,
+        withCredentials: me.options.withCredentials,
+        withoutFormatSuffix: true,
+        crossOrigin: me.options.crossOrigin,
+        headers: me.options.headers,
+        eventListeners: {
+            scope: me,
+            processCompleted: callback,
+            processFailed: callback
+        }, projection: me.options.projection
+    });
+    getMapStatusService.processAsync();
+  }
 
-    /**
-     * @function MapService.prototype.getTilesets
-     * @description 切片列表信息查询服务。
-     * @param {RequestCallback} callback - 回调函数。
-     * @returns {MapService} 获取服务信息。
-     */
-    getTilesets(callback) {
-        var me = this;
-        var tilesetsService = new TilesetsService(me.url, {
-            proxy: me.options.proxy,
-            withCredentials: me.options.withCredentials,
-            crossOrigin: me.options.crossOrigin,
-            headers: me.options.headers,
-            eventListeners: {
-                scope: me,
-                processCompleted: callback,
-                processFailed: callback
-            }
-        });
-        tilesetsService.processAsync();
-    }
+  /**
+   * @function MapService.prototype.getTilesets
+   * @description 切片列表信息查询服务。
+   * @param {RequestCallback} callback - 回调函数。
+   * @returns {MapService} 获取服务信息。
+   */
+  getTilesets(callback) {
+    var me = this;
+    var tilesetsService = new TilesetsService(me.url, {
+      proxy: me.options.proxy,
+      withCredentials: me.options.withCredentials,
+      crossOrigin: me.options.crossOrigin,
+      headers: me.options.headers,
+      eventListeners: {
+        scope: me,
+        processCompleted: callback,
+        processFailed: callback
+      }
+    });
+    tilesetsService.processAsync();
+  }
 }
