96f5ed7df7deb9eaeeedcc3c640220fd87602d47
luoxiao-supermap.com <luoxiao@supermap.com>
2023-01-11 14:15:00 +0800

    [fix]ISVJ-5321特殊字符字段过滤 review by qiw

--- src/common/util/FilterCondition.js
+++ src/common/util/FilterCondition.js
@@ -1,11 +1,38 @@
+function getParseSpecialCharacter() {
+  // 特殊字符字典
+  const directory = ['(', ')', '（', '）', ',', '，'];
+  const res = {};
+  directory.forEach((item, index) => {
+    res[item] = `$${index}`
+  });
+  return res;
+}
+
+function parseSpecialCharacter(str) {
+  const directory = getParseSpecialCharacter();
+  for (let key in directory) {
+    const replaceValue = directory[key];
+    const pattern = new RegExp(`\\${key}`, 'g');
+    // eslint-disable-next-line
+    while (pattern.test(str)) {
+      str = str.replace(pattern, replaceValue);
+    }
+  }
+  return str;
+}
+
 function parseCondition(filterCondition, keys) {
   const str = filterCondition.replace(/&|\||>|<|=|!/g, ' ');
   const arr = str.split(' ').filter((item) => item);
   let result = filterCondition;
   arr.forEach((item) => {
-    const key = startsWithNumber(item) && keys.find((val) => val === item);
-    if (key) {
+    const key = keys.find((val) => val === item);
+    if (startsWithNumber(item) && key) {
       result = result.replace(key, '$' + key);
+    }
+    if (key) {
+      const res = parseSpecialCharacter(key);
+      result = result.replace(key, res);
     }
   });
   return result;
@@ -13,12 +40,13 @@ function parseCondition(filterCondition, keys) {
 
 // 处理jsonsqlfeature, 加前缀
 function parseConditionFeature(feature) {
-  const copyValue = {};
-  for (const key in feature) {
+  let copyValue = {};
+  for (let key in feature) {
     let copyKey = key;
     if (startsWithNumber(key)) {
       copyKey = '$' + key;
     }
+    copyKey = parseSpecialCharacter(copyKey);
     copyValue[copyKey] = feature[key];
   }
   return copyValue;
