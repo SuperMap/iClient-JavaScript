dd5c4ded7cf52de5510a8a4de2f46c3458896cff
jinny <jinnanyang@supermap.com>
2023-03-02 20:29:02 +0800

    [fix]修复vt

--- examples-test/leaflet/01_layerServiceIT.js
+++ examples-test/leaflet/01_layerServiceIT.js
@@ -7,7 +7,7 @@ module.exports = {
         /*check element exist*/
         browser.waitForElementPresent('.leaflet-popup-content', 10000);
         /*check the info showing in leaflet-popup is equal to our expectation*/
-        var layerInfo = 'continent_T@World\nOcean_Label_1@World\nCapitals@World#3\nCapitals@World#2\nCapitals@World#1\nCountries@World#1\nChina_Boundary_nanhai@World\nChina_Boundary_1@World\nCountry_Label@World\nRivers@World\nChina_island_part@World\nLakes@World\nCountries@World#2\nCountries@World\nOceanL@World\ngl_latlong_1km_landcover@World';
+        var layerInfo = 'continent_T@World\nOcean_Label_1@World\nCapitals@World#3\nCapitals@World#2\nCapitals@World#1\nCountries@World\nChina_Boundary_nanhai@World\nChina_Boundary_1@World\nCountry_Label@World\nRivers@World\nChina_island_part@World\nLakes@World\nCountries@World#1\nCountries@World#2\nOceanL@World\ngl_latlong_1km_landcover@World';
         browser.expect.element('.leaflet-popup-content').text.to.equal(layerInfo);
         browser.pause(1000);
         browser.click('.leaflet-popup-close-button', function () {
