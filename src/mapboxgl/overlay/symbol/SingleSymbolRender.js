b288e153730d9c2f47dbd057c8faa8980652b57c
qiulinFV <qiulin@supermap.com>
2023-06-21 11:22:29 +0800

    [update]web符号示例优化；添加copyright；

--- src/mapboxgl/overlay/symbol/SingleSymbolRender.js
+++ src/mapboxgl/overlay/symbol/SingleSymbolRender.js
@@ -1,3 +1,6 @@
+/* Copyright© 2000 - 2023 SuperMap Software Co.Ltd. All rights reserved.
+ * This program are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at http://www.apache.org/licenses/LICENSE-2.0.html.*/
 /**
  * 单个符号
  * @returns {Object}
