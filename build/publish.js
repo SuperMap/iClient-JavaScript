54f37ff21e06cc4abcfefe0ce46cdc6fe3f9a0c3
songyumeng <songyumeng@supermap.com>
2023-06-23 00:06:04 +0800

    【fix】修复打包

--- build/publish.js
+++ build/publish.js
@@ -1,49 +1,16 @@
-var fs = require('fs');
-var path = require('path');
+const path = require('path');
 const fse = require('fs-extra')
 
-var key = process.argv[2] ? process.argv[2].replace("--", "") : "leaflet";
-var filePath = path.join(__dirname, "../dist");
-var destDir = path.join(__dirname, "../src/" + key + "/dist");
-if (!fs.existsSync(destDir)) {
-    fs.mkdirSync(destDir);
-}
-fs.readdir(path.join(filePath, key), function (err, files) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    files.forEach(function (fileName) {
-        if (fileName.indexOf(key) >= 0 && fileName.indexOf("include") == -1) {
-            var sourceFile = path.join(__dirname, "../dist/" + key, fileName);
-            var destPath = path.join(__dirname, "../src/" + key + "/dist", fileName);
-            var readStream = fs.createReadStream(sourceFile);
-            var writeStream = fs.createWriteStream(destPath);
-            readStream.pipe(writeStream);
-            return;
-        }
-        //workers
-        if (fs.statSync(path.join(filePath, key, fileName)).isDirectory()) {
-            if (!fs.existsSync(path.join(__dirname, "../src/" + key + "/dist/", fileName))) {
-                fs.mkdirSync(path.join(__dirname, "../src/" + key + "/dist/", fileName));
-            }
-            fs.readdir(path.join(filePath, key, fileName), function (err, subfiles) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                subfiles.forEach(function (subfileName) {
-                    var sourceFile = path.join(__dirname, "../dist/", key, fileName, subfileName);
-                    var destPath = path.join(__dirname, "../src/" + key + "/dist/", fileName, subfileName);
-                    var readStream = fs.createReadStream(sourceFile);
-                    var writeStream = fs.createWriteStream(destPath);
-                    readStream.pipe(writeStream);
-                })
-            })
-        }
-    })
-});
+const key = process.argv[2] ? process.argv[2].replace("--", "") : "leaflet";
+const sourceDir = path.join(__dirname, "../dist/" + key);
+const destDir = path.join(__dirname, "../src/" + key + "/dist");
 
+fse.removeSync(destDir);
+fse.copySync(sourceDir, destDir, {
+    filter: (src) => {
+        return !(src.indexOf("include-") >= 0 || src.indexOf("resources") >= 0)
+    }
+});
 if (key === 'mapboxgl') {
     // 拷贝resources文件夹到src/mapboxgl
     const source = path.join(__dirname, "../dist/resources");
