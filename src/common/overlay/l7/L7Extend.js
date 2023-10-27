1016f0a07c905db891ec77a2178b61c3e158e78b
luoxiao-supermap.com <luoxiao@supermap.com>
2023-10-27 15:59:41 +0800

    [fix]修改l7例子

--- src/common/overlay/l7/L7Extend.js
+++ src/common/overlay/l7/L7Extend.js
@@ -8,7 +8,6 @@ export function getL7Scene(map, type = 'Maplibre') {
   return new L7.Scene({
     id: map.getContainer().id,
     pickBufferScale: 1.0,
-    isIClient: true,
     map: new L7[type]({
       mapInstance: map
     })
