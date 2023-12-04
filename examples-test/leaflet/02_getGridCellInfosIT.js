5e8fa476e1e81a22f27b4cc0d0dd45e1ce5dae63
jinny <jinnanyang@supermap.com>
2023-12-04 09:40:27 +0800

    [fix]VT review by qiwei

--- examples-test/leaflet/02_getGridCellInfosIT.js
+++ examples-test/leaflet/02_getGridCellInfosIT.js
@@ -1,31 +1,26 @@
 var commonTools = require('../base/commonTools');
 module.exports = {
-    after: function (browser) {
-        console.log('Closing down...');
-        browser.end();
-      },
-    'leaflet_02_getGridCellInfos': function (browser) {
-        var type = 'leaflet';
-        var exampleName = '02_getGridCellInfos';
-        commonTools.openExampleAndLoadMap(browser, type, exampleName);
-        /*check elements exist*/
-        browser.click('#map');
-        browser.waitForElementPresent('.leaflet-pane.leaflet-marker-pane', 10000);
-        browser.waitForElementPresent('.leaflet-pane.leaflet-marker-pane img', 10000);
-        browser.waitForElementPresent('.leaflet-popup-content', 10000);
-        browser.expect.element('.leaflet-popup-content').text.to.be.contain('栅格查询结果');
-        browser.expect.element('.leaflet-popup-content').text.to.be.contain('column');
-        browser.expect.element('.leaflet-popup-content').text.to.be.contain('row');
-        browser.expect.element('.leaflet-popup-content').text.to.be.contain('value');
-        browser.click('.leaflet-popup-close-button', function () {
-            browser.waitForElementNotPresent('.leaflet-popup-content', 10000);
-        });
-        //测试版权点击的正确性
-        //commonTools.verifyCopyrightOfLeaflet(browser);
-        // browser.pause(1000);
-        // browser.end();
-    }
+  after: function (browser) {
+    console.log('Closing down...');
+    browser.end();
+  },
+  'leaflet_02_getGridCellInfos': function (browser) {
+    var type = 'leaflet';
+    var exampleName = '02_getGridCellInfos';
+    commonTools.openExampleAndLoadMap(browser, type, exampleName);
+    /*check elements exist*/
+    browser
+      .waitForElementVisible('.leaflet-draw-draw-marker')
+      .click('.leaflet-draw-draw-marker')
+      .click('#map')
+      .click('.panel-group.sidebar-config-wrap .btn-primary')
+      .waitForElementVisible('#tableContainerAll .table-container')
+      .expect.element('#tableContainerAll tbody tr td:nth-child(5)')
+      .text.to.equal('{"red":25,"green":38,"blue":0,"alpha":255}');
+
+    //测试版权点击的正确性
+    //commonTools.verifyCopyrightOfLeaflet(browser);
+    // browser.pause(1000);
+    // browser.end();
+  }
 };
-
-
-
