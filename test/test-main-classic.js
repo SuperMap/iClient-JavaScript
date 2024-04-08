99a2c050af65d2a9117f1a53b88986046c44906d
luoxiao <luoxiao@supermap.com>
2024-04-08 15:16:55 +0800

    [fix]UT

--- test/test-main-classic.js
+++ test/test-main-classic.js
@@ -6,3 +6,4 @@ import './classic/services/AddressMatchServiceSpec.js';
 import './classic/services/ProcessingServiceSpec.js';
 import './classic/services/DatasetServiceSpec.js';
 import './classic/services/DatasourceServiceSpec.js';
+import './classic/namespaceSpec.js';
