495628b0a90644f0a8d995a8e225dacc76a86021
luoxiao <luoxiao@supermap.com>
2024-04-08 15:26:02 +0800

    [fix]UT

--- test/classic/namespaceSpec.js
+++ test/classic/namespaceSpec.js
@@ -1,21 +1,21 @@
-import {FetchRequest, VectorClipJobsParameter} from '../../../src/classic/namespace';
+import { FetchRequest, VectorClipJobsParameter } from '../../src/classic/namespace';
 
 describe('namespace', () => {
-    var originaTimeout;
-    beforeEach(() => {
-        originaTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
-    });
-    afterEach(() => {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = originaTimeout;
-    });
+  var originaTimeout;
+  beforeEach(() => {
+    originaTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
+  });
+  afterEach(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originaTimeout;
+  });
 
-    it('FetchRequest, VectorClipJobsParameter', () => {
-        expect(FetchRequest).not.toBeNull();
-        expect(VectorClipJobsParameter).not.toBeNull();
-        expect(typeof FetchRequest).toBe('function');
-        expect(typeof VectorClipJobsParameter).toBe('function');    
-        expect(typeof SuperMap.FetchRequest).toBe('function');
-        expect(typeof SuperMap.VectorClipJobsParameter).toBe('function');    
-    });
+  it('FetchRequest, VectorClipJobsParameter', () => {
+    expect(FetchRequest).not.toBeNull();
+    expect(VectorClipJobsParameter).not.toBeNull();
+    expect(typeof FetchRequest).toBe('function');
+    expect(typeof VectorClipJobsParameter).toBe('function');
+    expect(typeof SuperMap.FetchRequest).toBe('function');
+    expect(typeof SuperMap.VectorClipJobsParameter).toBe('function');
+  });
 });
