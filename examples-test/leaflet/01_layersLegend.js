21a2fb42b0b93195f03c3cc5f022a8312691a59d
jinny <jinnanyang@supermap.com>
2024-05-31 16:33:12 +0800

    [fix]维护vt review by qw

--- examples-test/leaflet/01_layersLegend.js
+++ examples-test/leaflet/01_layersLegend.js
@@ -8,21 +8,12 @@ module.exports = {
     var type = 'leaflet';
     var exampleName = '01_layersLegend';
     commonTools.openExampleAndLoadMap(browser, type, exampleName);
-    /*check element exist*/
-    // browser.waitForElementPresent('.leaflet-popup-content', 10000);
-    /*check the info showing in leaflet-popup is equal to our expectation*/
-    browser.elements('css selector', '.panel.panel-primary', function (result) {
-      console.log('----Hope number of panel to be 2, actual is ' + result.value.length);
-    });
+    browser.expect.elements('.panel.panel-primary').count.to.equal(2).before(5000);
     browser.waitForElementVisible('#mapLegend>div');
+    browser.waitForElementVisible('#layersList label');
     browser.expect.elements('#layersList label').count.to.equal(9);
-    browser.click('.panel.panel-primary #layersList input');
-    browser.click('.btn.btn-default');
-    browser.waitForElementVisible('#mapLegend>div');
-    browser.pause(1000);
-    browser.expect.elements('#mapLegend>div').count.to.equal(4);
-    browser.expect.element('#mapLegend div:nth-child(1)').text.to.equal('CoordsysLabel@Jingjin');
-    browser.expect.element('#mapLegend div:nth-child(3)').text.to.equal('Landuse_R@Jingjin#1');
+    browser.expect.elements('#mapLegend>div').count.to.equal(2);
+    browser.expect.element('#mapLegend div:nth-child(1)').text.to.equal('Landuse_R@Jingjin#1');
     browser.expect
       .elements(
         'img[src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAAa0lEQVR4XmNgoAWYsXz3f0IYXQ9WAFOMC4DkInMbwBhdLwogxiAYwGsY3QyCeQsZo5sBBtgMErETgWN0QLRB6JrR+UQbhA4oNohir8EANkNAgGSDcAGSDaKai6hmEC5AP4PQswE+jG4GxQAAXHqikucMHGcAAAAASUVORK5CYII="]'
