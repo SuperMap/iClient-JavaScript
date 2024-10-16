d4745532e2a699416960deaecd48712e83eeae88
songyumeng <songyumeng@supermap.com>
2024-10-16 17:36:25 +0800

    【example】移除栅格分析示例

--- examples/mapboxgl/config.js
+++ examples/mapboxgl/config.js
@@ -21,13 +21,13 @@ var exampleConfig = {
                         thumbnail: 'mb_TileLayer.png',
                         fileName: '01_tiledMapLayer'
                     },
-                    {
-                        name: '地图-栅格分析',
-                        name_en: 'map rasterFunction',
-                        version: '10.0.0',
-                        thumbnail: 'tiledMapLayerRasterfunction.png',
-                        fileName: '01_tiledMapLayer_rasterfunction'
-                    },
+                    // {
+                    //     name: '地图-栅格分析',
+                    //     name_en: 'map rasterFunction',
+                    //     version: '10.0.0',
+                    //     thumbnail: 'tiledMapLayerRasterfunction.png',
+                    //     fileName: '01_tiledMapLayer_rasterfunction'
+                    // },
                     {
                         name: '地图信息',
                         name_en: 'map query service',
