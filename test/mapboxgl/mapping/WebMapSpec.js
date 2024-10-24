4a997a511466458c8deaa446c6d45238f758ab85
luoxiao <luoxiao@supermap.com>
2024-10-24 14:49:40 +0800

    [fix]webmap对接webmercator全球剖分瓦片

--- test/mapboxgl/mapping/WebMapSpec.js
+++ test/mapboxgl/mapping/WebMapSpec.js
@@ -120,6 +120,30 @@ describe('mapboxgl_WebMap', () => {
             done();
         });
     });
+
+    it('add zxytile layer', (done) => {
+      spyOn(FetchRequest, 'get').and.callFake((url) => {
+        if (url.indexOf('portal.json') > -1) {
+          return Promise.resolve(new Response(JSON.stringify(iportal_serviceProxy)));
+        } else if (url.indexOf('/map.json') > -1) {
+          return Promise.resolve(new Response(datavizWebmap_ZXYTILE));
+        }
+        return Promise.resolve();
+      });
+      datavizWebmap = new WebMap(
+        'test',
+        {
+          target: 'map',
+          serverUrl: 'http://fake/fakeiportal',
+          withCredentials: false
+        }
+      );
+      datavizWebmap.on('addlayerssucceeded', ({ layers }) => {
+        expect(layers.length).toBe(2);
+        done();
+      });
+    });
+
     it('setWebMapOptions', (done) => {
         let options = {
             server: server
