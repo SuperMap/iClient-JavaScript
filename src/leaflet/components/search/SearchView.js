336de4707457b8e8cd3a185dc578df42f191951e
chenxianhui <chenxianhui@supermap.com>
2024-11-04 14:07:49 +0800

    ICL-1554 修复leaflet组件：地址匹配与图层要素查询，被查询的图层要素在页面消失的问题 review by luox

--- src/leaflet/components/search/SearchView.js
+++ src/leaflet/components/search/SearchView.js
@@ -664,8 +664,10 @@ export var SearchView = ComponentsViewBase.extend({
             this.map.closePopup();
             //若当前是查询图层的结果，则不删除图层，只修改样式
             !this.isSearchLayer && this.map.removeLayer(this.searchResultLayer);
-            this._selectMarkerFeature && this.map.removeLayer(this._selectMarkerFeature);
-            this._selectFeaturethis && this.map.removeLayer(this._selectFeature);
+            if (this._selectMarkerFeature) {
+              this.map.removeLayer(this._selectMarkerFeature);
+              this.isSearchLayer && this._selectFeature.addTo(this.map);
+            }
             this._selectMarkerFeature = null;
             this._selectFeature = null;
             this.searchResultLayer = null;
@@ -737,7 +739,7 @@ export var SearchView = ComponentsViewBase.extend({
                 return L.marker(latlng, {
                     icon: L.divIcon({
                         className: 'component-select-marker-icon',
-                        iconAnchor: [15, 0]
+                        iconAnchor: [15, 42]
                     })
                 })
             },
@@ -755,7 +757,8 @@ export var SearchView = ComponentsViewBase.extend({
                 attributes: layer.feature.properties
             })).getElement()
         }, {
-            closeOnClick: false
+            closeOnClick: false,
+            offset: [0, -36]
         }).openPopup().addTo(this.map);
 
         this._flyToBounds(this.searchResultLayer.getBounds());
