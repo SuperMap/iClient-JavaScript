c87d1c1ad9cfdc91c13765f78ab5f2ba44eea1b9
qiwei <qiwei@supermap.com>
2025-01-13 11:50:10 +0800

    ol mapbox webmap 兼容非restjsr 不处理后缀

--- src/common/mapping/WebMapV2.js
+++ src/common/mapping/WebMapV2.js
@@ -349,8 +349,9 @@ export function createWebMapV2Extending(SuperClass, { MapManager, mapRepo, DataF
       if (url.indexOf('/restjsr/') > -1 && !/\/style\.json$/.test(url)) {
         url += '/style.json';
       }
+      const withoutFormatSuffix = url.indexOf('/restjsr/') === -1;
       this.webMapService
-        .getMapBoxStyle(url)
+        .getMapBoxStyle(url, withoutFormatSuffix)
         .then(
           (style) => {
             const sourceIds = Object.keys(style.sources);
