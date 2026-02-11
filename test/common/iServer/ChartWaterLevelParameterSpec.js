e7157103833a0940f5e13ef9a2b013704078582f
chenxianhui <chenxianhui@supermap.com>
2026-02-11 15:12:22 +0800

    fix ut

--- test/common/iServer/ChartWaterLevelParameterSpec.js
+++ test/common/iServer/ChartWaterLevelParameterSpec.js
@@ -4,22 +4,25 @@ describe('ChartWaterLevelParameter', () => {
     it('constructor, destroy', () => {
         var options = {
             dataSource: 'datasrouce',
-            bounds: [113.6307273864746, 22.780766677856448, 113.6358772277832, 22.785916519165042],
+            coordinates: [113.6307273864746, 22.780766677856448],
+            currentTime: '20251224T000000Z',
         };
         var parameter = new ChartWaterLevelParameter(options);
         expect(parameter).not.toBeNull();
         expect(parameter.dataSource).toBe('datasrouce');
-        expect(parameter.bounds).toEqual([113.6307273864746, 22.780766677856448, 113.6358772277832, 22.785916519165042]);
+        expect(parameter.coordinates).toEqual([113.6307273864746, 22.780766677856448]);
         expect(parameter.dataset).toBe('S104Position4326');
         expect(parameter.waterLevelDataset).toBe('S104WaterLevel');
         expect(parameter.timeDataset).toBe('S104Time');
         expect(parameter.timeIdKey).toBe('TIMEID');
+        expect(parameter.currentTime).toBe('20251224T000000Z');
         parameter.destroy();
         expect(parameter.dataSource).toBeNull();
-        expect(parameter.bounds).toBeNull();
+        expect(parameter.coordinates).toBeNull();
         expect(parameter.dataset).toBeNull();
         expect(parameter.waterLevelDataset).toBeNull();
         expect(parameter.timeDataset).toBeNull();
         expect(parameter.timeIdKey).toBeNull();
+        expect(parameter.currentTime).toBeNull();
     });
 });
