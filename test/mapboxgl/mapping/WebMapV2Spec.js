f22cd18ddad744eb5c0a79262896016f9be75937
luoxiao <luoxiao@supermap.com>
2026-07-02 10:08:56 +0800

    [fix]UT AI-GEN: 0% MiniMax-M2.7

--- test/mapboxgl/mapping/WebMapV2Spec.js
+++ test/mapboxgl/mapping/WebMapV2Spec.js
@@ -1402,7 +1402,7 @@ it('add rangeLayer last end === fieldValue', (done) => {
         }
       ];
       datavizWebmap.cleanLayers();
-      expect(getSourceSpy).toHaveBeenCalledTimes(2);
+      expect(getSourceSpy).toHaveBeenCalledTimes(3);
       expect(removeSourceSpy).toHaveBeenCalledTimes(1);
       expect(datavizWebmap._cacheCleanLayers.length).toBe(0);
       done();
