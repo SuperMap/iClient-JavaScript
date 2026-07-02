91706c5d5b691e36c89539e433784dfd1ee53336
luoxiao <luoxiao@supermap.com>
2026-07-02 10:08:56 +0800

    [fix]UT AI-GEN: 0% MiniMax-M2.7

--- test/mapboxgl/mapping/WebMapV2Spec.js
+++ test/mapboxgl/mapping/WebMapV2Spec.js
@@ -1401,7 +1401,7 @@ describe('mapboxgl_WebMapV2', () => {
         }
       ];
       datavizWebmap.cleanLayers();
-      expect(getSourceSpy).toHaveBeenCalledTimes(2);
+      expect(getSourceSpy).toHaveBeenCalledTimes(3);
       expect(removeSourceSpy).toHaveBeenCalledTimes(1);
       expect(datavizWebmap._cacheCleanLayers.length).toBe(0);
       done();
