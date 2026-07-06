ade2d4ba0821c4961fdd5eaa02615c75101e52a5
songyumeng <songyumeng@supermap.com>
2026-07-06 10:22:18 +0800

    【version】12.1.1
    AI-GEN: 0%

--- examples/js/include-web.js
+++ examples/js/include-web.js
@@ -240,6 +240,6 @@
     });
     window.isLocal = false;
     window.server = document.location.toString().match(/file:\/\//) ? "http://localhost:8090" : document.location.protocol + "//" + document.location.host;
-    window.version = "12.1.0-r";
+    window.version = "12.1.1-dev";
     window.preRelease = "";
 })();
